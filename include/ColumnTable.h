2551b45f48a73b62e1230d373a8c8f19609f7f34
lemon0910 <lemon_wonder@163.com>
2015-06-26 17:12:40 +0800

    modify

--- include/ColumnTable.h
+++ include/ColumnTable.h
@@ -10,19 +10,19 @@ class ColumnTable
 {
 private:
 
-    typedef std::vector<std::pair<std::string, size_t> > vector_t;
+    typedef std::vector<std::pair<std::string, uint64_t> > vector_t;
 
 public:
 
     ColumnTable(std::string tableName);
     virtual ~ColumnTable();
-    void insertOneDGroupKey(void* insertPtr, std::string columnName, size_t type);
-    std::pair<void*, size_t> getOneDGroupKey(std::string columnName);
-    std::vector<uint64_t> updateOneDGroupKey(void* updatePtr, std::string columnName, size_t type);
+    void insertOneDGroupKey(void* insertPtr, std::string columnName, uint64_t type);
+    std::pair<void*, uint64_t> getOneDGroupKey(std::string columnName);
+    std::vector<uint64_t> updateOneDGroupKey(void* updatePtr, std::string columnName, uint64_t type);
     std::string getTableName();
     void print();
 
-    inline size_t getPos(std::string columnName)//precise search, if not exist, return -1
+    inline uint64_t getPos(std::string columnName)//precise search, if not exist, return -1
     {
         for(vector_t::iterator itr = m_indexVector.begin(); itr != m_indexVector.end();
             itr++)
--- src/ColumnTable.cc
+++ src/ColumnTable.cc
@@ -1,6 +1,6 @@
 #include <ColumnTable.h>
 
-ColumnTable::ColumnTable(string tableName):m_tableName(tableName)
+ColumnTable::ColumnTable(std::string tableName):m_tableName(tableName)
 {
 }
 
@@ -10,55 +10,67 @@ ColumnTable::~ColumnTable()
 	{
 		switch(m_indexVector[i].second)
 		{
-			case 0:{
+			case 0:
+            {
 				DGroupKey<string>* string_ptr = (DGroupKey<string>*)m_columns[i];
 				delete string_ptr;
-			}break;
-			case 1:{
+			}
+            break;
+			case 1:
+            {
 				DGroupKey<int>* int_ptr = (DGroupKey<int>*)m_columns[i];
 				delete int_ptr;
-			}break;
-			case 2:{
+			}
+            break;
+			case 2:
+            {
 				DGroupKey<double>* double_ptr = (DGroupKey<double>*)m_columns[i];
 				delete double_ptr;
-			}break;
+			}
+            break;
 		}
 	}
 	m_columns.clear();
 	m_indexVector.clear();
 }
 
-void ColumnTable::insertOneDGroupKey(void* insertPtr, string columnName, size_t type)
+void ColumnTable::insertOneDGroupKey(void* insertPtr, std::string columnName, uint64_t type)
 {
-    RWMutexLockGuard tmpLock(_mutex , Choice::choiceWrite);
+    RWMutexLockGuard tmpLock(_mutex , WRITE);
 	m_columns.push_back(insertPtr);
 	m_indexVector.push_back(make_pair(columnName, type));
 }
 
-pair<void*, size_t> ColumnTable::getOneDGroupKey(string columnName)
+pair<void*, uint64_t> ColumnTable::getOneDGroupKey(std::string columnName)
 {
-	size_t pos = getPos(columnName);
+	uint64_t pos = getPos(columnName);
 	void* result_ptr = m_columns[pos];
-	size_t result_type = m_indexVector[pos].second;
+	uint64_t result_type = m_indexVector[pos].second;
 	return make_pair(result_ptr, result_type);
 }
 
-vector<uint64_t> ColumnTable::updateOneDGroupKey(void* updatePtr, string columnName, size_t type)
+vector<uint64_t> ColumnTable::updateOneDGroupKey(void* updatePtr, string columnName, uint64_t type)
 {
-    RWMutexLockGuard tmpLock(_mutex , Choice::choiceWrite);
-	pair<void*, size_t> formerColumn = getOneDGroupKey(columnName);
+    RWMutexLockGuard tmpLock(_mutex , WRITE);
+	pair<void*, uint64_t> formerColumn = getOneDGroupKey(columnName);
 	switch(type)
 	{
-		case 0:{
+		case 0:
+        {
 			DGroupKey<string>* string_ptr = (DGroupKey<string>*)formerColumn.first;
 			return string_ptr->insertUpdatedData(((DGroupKey<string>*)updatePtr)->getDictionary(), ((DGroupKey<string>*)updatePtr)->getOffset(), ((DGroupKey<string>*)updatePtr)->getPosition());
-		}break;
-		case 1:{
+		}
+        break;
+		case 1:
+        {
 			return ((DGroupKey<int>*)formerColumn.first)->insertUpdatedData(((DGroupKey<int>*)updatePtr)->getDictionary(), ((DGroupKey<int>*)updatePtr)->getOffset(), ((DGroupKey<int>*)updatePtr)->getPosition());
-		}break;
-		case 2:{
+		}
+        break;
+		case 2:
+        {
 			return ((DGroupKey<double>*)formerColumn.first)->insertUpdatedData(((DGroupKey<double>*)updatePtr)->getDictionary(), ((DGroupKey<double>*)updatePtr)->getOffset(), ((DGroupKey<double>*)updatePtr)->getPosition());
-		}break;
+		}
+        break;
 	}
 }
 
